31f79500def21a226bea2c7305c8223a63a6f5d5
Ольга Пономарева <olia.v.ponomareva@gmail.com>
2024-11-02 10:51:46 +0300

    Домашнее задание от 27.10

---
+++ 2024.10.27/1.py
@@ -0,0 +1,15 @@
+name = input('Введите имя: ')
+sname = input('Введите фамилию: ')
+age = input('Введите год рождения: ')
+
+print('\n',name,sname+',',2024-int(age))
+
+# Результат:
+
+#Введите имя: Olga
+# Введите фамилию: Ponomareva
+# Введите год рождения: 1990
+
+# Olga Ponomareva, 34
+
+
